5793f05e716ad8584212847c8dd4220ee374359e
titanium901 <nemes901@hotmail.com>
2020-06-08 17:13:10 +0300

    Создание VCFactory и настройка TabBara

---
+++ MVVM-Covid19App/InfoScreen/InfoViewController.swift
@@ -0,0 +1,16 @@
+//
+//  InfoViewController.swift
+//  MVVM-Covid19App
+//
+//  Created by Iury Popov on 08.06.2020.
+//  Copyright © 2020 Iurii Popov. All rights reserved.
+//
+
+import UIKit
+
+class InfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ MVVM-Covid19App/MapScreen/MapViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MapViewController.swift
+//  MVVM-Covid19App
+//
+//  Created by Iury Popov on 08.06.2020.
+//  Copyright © 2020 Iurii Popov. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
