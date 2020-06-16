f8bb1f5388e7bb7a9b54ae1329c3bc6c40ec91fc
titanium901 <nemes901@hotmail.com>
2020-06-16 17:03:27 +0300

    Удаление лишнего из appDelegate

--- MVVM-Covid19App/App/SceneDelegate.swift
+++ MVVM-Covid19App/App/SceneDelegate.swift
@@ -23,6 +23,5 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         window?.rootViewController = viewControllerFactory.makeRootViewController()
         window?.makeKeyAndVisible()
-        
     }
 }
